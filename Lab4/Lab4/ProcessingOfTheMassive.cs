b7d6986fa11b7028d3bd802642afe1647253aeef
Rise <ritm-temp@hotmail.com>
2018-03-06 10:40:47 +0600

    Lab4 v0.5

---
+++ Lab4/Lab4/ProcessingOfTheMassive.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class ProcessingOfTheMassive
+    {
+        public int[] FindSumClosestToMaxNumber(int[] massiveOfNumbers)
+        {
+            int MaxNumber = MaxNumberInMassive(massiveOfNumbers);
+            int[] firstElem= {massiveOfNumbers[0],0 };
+            int[] secondElem = { massiveOfNumbers[0], 0 };
+            for(int i = 0; i < massiveOfNumbers.Length; i++)
+            {
+
+            }
+            return massiveOfNumbers;
+        }
+        private int MaxNumberInMassive(int[] massiveOfNumbers)
+        {
+            int MaxNumber = massiveOfNumbers[0];
+            for(int i = 0; i < massiveOfNumbers.Length; i++)
+            {
+                if (MaxNumber < massiveOfNumbers[i])
+                {
+                    MaxNumber = massiveOfNumbers[i];
+                }
+            }
+            return MaxNumber;
+        }
+        private int[] Swap2NumbersInMassive(int[] massiveOfNumbers, int indexOf1stElem,int indexOf2ndElem)
+        {
+            int saverOf1stElem = massiveOfNumbers[indexOf1stElem];
+            massiveOfNumbers[indexOf1stElem] = massiveOfNumbers[indexOf2ndElem];
+            massiveOfNumbers[indexOf1stElem] = saverOf1stElem;
+            return massiveOfNumbers;
+        }
+    }
+}
